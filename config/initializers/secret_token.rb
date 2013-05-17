01a4496bfb38ebf7d792f584eaf4828283050fce
rshiva <nadarshiva.r@gmail.com>
2013-05-17 13:08:25 +0530

    made a new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VehicleComplaints::Application.config.secret_token = 'ebd626392ee445f82492c861a9b208281bc4415a17e7508d5136fb9c7246c83b080df7e5eea7af532212235d48d2bc106b596fc7c000c3ab6010295eee9ebe6a'
