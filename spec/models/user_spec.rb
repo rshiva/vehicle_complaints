1c50b5ab82e84b2f22230f9bf297067803e03817
rshiva <nadarshiva.r@gmail.com>
2013-05-23 09:16:54 +0530

    refactor user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,20 +8,17 @@ describe User do
 	end
   
   it "should require name" do
-  	no_name_user=User.new(:email => "test@gmail.com" , mobile: "123456780")
-  	no_name_user.should_not be_valid
+    FactoryGirl.build(:user, :name => nil , mobile: "123456780").should_not be_valid
   end
 
 
   it "should require email" do
-  	no_email_user=User.new(:name => "test" , mobile: "123456780")
-  	no_email_user.should_not be_valid
+    FactoryGirl.build(:user, :email => nil , mobile: "123456780").should_not be_valid
   end
 
 
   it "should require mobile number" do
-  	no_mobile_number_user=User.new(:email => "test@gmail.com" , name: "tetst")
-  	no_mobile_number_user.should_not be_valid
+    FactoryGirl.build(:user , mobile: nil).should_not be_valid
   end
 
 
